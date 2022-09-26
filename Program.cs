ccdbf01f0b2cb1ff5de2252a944200d8de12d850
Cesar Jack-Vallee <cesarjackv@gmail.com>
2022-09-25 20:53:50 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿List<object> obj = new List<object>();
+obj.Add(7);
+obj.Add(28);
+obj.Add(-1);
+obj.Add(true);
+obj.Add("chair");
+
+int sum = 0;
+
+foreach(object item in obj){
+    Console.WriteLine(item);
+    if(item is int){
+        sum += (int)item;
+    }
+}
+
+Console.WriteLine(sum);
